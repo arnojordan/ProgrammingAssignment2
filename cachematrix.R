e9f53aba971c767563189e5d9c9df1e6fcbf332c
Dr. Arno Jordan <arno.jordan@gft.com>
2015-05-22 22:29:40 +0200

    First try

--- cachematrix.R
+++ cachematrix.R
@@ -2,6 +2,7 @@
 ## functions do
 
 ## Write a short comment describing this function
+## analogous to makeVector
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -9,6 +10,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##analogous to cachemean
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
